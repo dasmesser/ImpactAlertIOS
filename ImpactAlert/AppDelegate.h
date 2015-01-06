6b2af720d4a20463066005b67e52bb654af534ee
sayra arely ysikawa <petitfantom-7@Air-de-say.lan>
2015-01-06 02:23:50 -0600

    Initial Commit

---
+++ ImpactAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImpactAlert
+//
+//  Created by sayra arely ysikawa on 06/01/15.
+//  Copyright (c) 2015 ImpacAlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
