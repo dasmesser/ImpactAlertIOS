006ea1ef7b538104337c228b700b8a6111c1bdc2
Jair Ramirez <jorgejairsmln@hotmail.com>
2015-01-23 22:57:27 -0600

    Before changing the textField to a TextView

---
+++ ConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigurationViewController.h
+//  ImpactAlert
+//
+//  Created by sayra arely ysikawa on 15/01/15.
+//  Copyright (c) 2015 ImpacAlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigurationViewController : UIViewController
+
+@end
---
+++ EmergencyContactSelectionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmergencyContactSelectionTableViewController.h
+//  ImpactAlert
+//
+//  Created by sayra arely ysikawa on 20/01/15.
+//  Copyright (c) 2015 ImpacAlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmergencyContactSelectionTableViewController : UITableViewController
+
+@end
